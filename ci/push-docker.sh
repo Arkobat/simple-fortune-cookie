9deaa0bbbdf1905ec5df22831b0e91f09d88100e
Kristian Jørgensen <mail@kristianj.dk>
2021-08-22 10:28:41 +0000

    Fix image name in push

--- ci/push-docker.sh
+++ ci/push-docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 docker login --username "$docker_username" --password "$docker_password"
-docker push "$docker_username/frontend:1.0-${GIT_COMMIT::4}" 
-docker push "$docker_username/frontend:latest" 
+docker push "$docker_username/sft-frontend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/sft-frontend:latest" 
 
-docker push "$docker_username/backend:1.0-${GIT_COMMIT::4}" 
-docker push "$docker_username/backend:latest" 
+docker push "$docker_username/sft-backend:1.0-${GIT_COMMIT::4}" 
+docker push "$docker_username/sft-backend:latest" 
 
 wait
